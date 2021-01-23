0c211581e250590fdf1eb4a913f08e609d6926d6
Keiichi Hirobe <chalenge.akane@gmail.com>
2021-01-23 13:44:59 +0900

    add some test

--- fifo/persistent_singly_list_test.go
+++ fifo/persistent_singly_list_test.go
@@ -6,7 +6,6 @@ func TestPListCopy(t *testing.T) {
 	l := PList{}
 	l.PushBack(1)
 	l.PushBack(2)
-	// l2 never change fron now
 	l2 := l
 	checkPList(t, &l, []interface{}{1, 2})
 	checkPList(t, &l2, []interface{}{1, 2})
@@ -23,4 +22,20 @@ func TestPListCopy(t *testing.T) {
 	l.PushBack(4)
 	checkPList(t, &l, []interface{}{4})
 	checkPList(t, &l2, []interface{}{1, 2})
+}
+
+func TestPlistCopy2(t *testing.T) {
+	l := PList{}
+	l.PushBack(1)
+	l.PushBack(2)
+	l.PushBack(3)
+	l2 := l
+	checkPList(t, &l, []interface{}{1, 2, 3})
+	checkPList(t, &l2, []interface{}{1, 2, 3})
+	l.PushBack(4)
+	checkPList(t, &l, []interface{}{1, 2, 3, 4})
+	checkPList(t, &l2, []interface{}{1, 2, 3})
+	l2.PushBack(5)
+	checkPList(t, &l, []interface{}{1, 2, 3, 4})
+	checkPList(t, &l2, []interface{}{1, 2, 3, 5})
 }
--- fifo/two_list_test.go
+++ fifo/two_list_test.go
@@ -24,3 +24,27 @@ func TestTwoListCopy(t *testing.T) {
 	checkTwoList(t, &l, []interface{}{4})
 	checkTwoList(t, &l2, []interface{}{1, 2})
 }
+
+func TestTwoListCopy2(t *testing.T) {
+	l := TwoList{}
+	l.PushBack(1)
+	l.PushBack(2)
+	l.PushBack(3)
+	l2 := l
+	checkTwoList(t, &l, []interface{}{1, 2, 3})
+	checkTwoList(t, &l2, []interface{}{1, 2, 3})
+	l.PushBack(4)
+	checkTwoList(t, &l, []interface{}{1, 2, 3, 4})
+	checkTwoList(t, &l2, []interface{}{1, 2, 3})
+	l2.PushBack(5)
+	checkTwoList(t, &l, []interface{}{1, 2, 3, 4})
+	checkTwoList(t, &l2, []interface{}{1, 2, 3, 5})
+	checkEqualEl(t, l.PopFront().Value, 1)
+	checkEqualEl(t, l.PopFront().Value, 2)
+	checkEqualEl(t, l.PopFront().Value, 3)
+	checkEqualEl(t, l.PopFront().Value, 4)
+	checkEqualEl(t, l2.PopFront().Value, 1)
+	checkEqualEl(t, l2.PopFront().Value, 2)
+	checkEqualEl(t, l2.PopFront().Value, 3)
+	checkEqualEl(t, l2.PopFront().Value, 5)
+}
